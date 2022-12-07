cf68449f33da782aaed9c128e4e4e0a94b73811a
Prem Kumar <personalaarav@gmail.com>
2022-12-07 23:40:06 +0530

    I have made some changes in the code

--- prime.js
+++ prime.js
@@ -1,8 +1,10 @@
 function isPrime(num) {
+  let count = 0;
+  
   if (num == 1) {
     return false
   }
-  let count = 0;
+
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i == 0) {
       count++;
@@ -10,7 +12,8 @@ function isPrime(num) {
   } if (count == 0) {
     return true;
   } return false;
+  
 }
 
 let n = 13;
-console.log(isPrime(n));
+console.log(n,"-",isPrime(n)); 
