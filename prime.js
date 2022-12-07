7056ffb9e76e52e9a564c08be6df1c71901a68e0
unknown <harshalmusmade410@gmail.com>
2022-12-07 14:31:06 +0530

    This is the first commit

---
+++ prime.js
@@ -0,0 +1,15 @@
+function isPrime(num) {
+  if (num == 1) {
+    return false
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i == 0) {
+      count++;
+    }
+  } if (count == 0) {
+    return true;
+  } return false;
+}
+
+let n = 13;
+console.log(isPrime(n));
